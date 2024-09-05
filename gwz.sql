ef8ab0f8f6704c7e467568308c940e6bb0281d6a
DenisseAlba <dcuellarfr@gmail.com>
2024-09-05 18:17:13 -0500

    Update gwz.sql
    Update

--- gwz.sql
+++ gwz.sql
@@ -1,3 +1,3 @@
-SELECT date_date, round(sum(turnover * qty),2) AS TotalCost
+SELECT date_date, round(sum(turnover * qty),2) AS TotalCost, round(sum(purchase_cost),2) As total_PC
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
 Group by date_date
